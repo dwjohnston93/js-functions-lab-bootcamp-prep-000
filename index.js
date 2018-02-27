55d1bbc465ff8fe4211d0a01c251841cd43d64fa
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-02-27 20:16:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ function happyHolidays() {
   return "Happy holidays!"; 
 }
 
-function happyHolidaysTo(holiday, name) {
+function happyHolidayTo(holiday, name) {
   return `Happy ${holiday}, ${name}!`;
 }
 
 function holidayCountdown(days, holiday) {
-  return `It's ${days} until ${holiday}!`;
+  return `It's ${days} days until ${holiday}!`;
 }
