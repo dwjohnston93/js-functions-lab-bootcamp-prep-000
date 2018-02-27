1c2233381560c7bff51877c24b40f70928f467bf
DANIEL JOHNSTON <danjohnston93@cox.net>
2018-02-27 20:20:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ function happyHolidays() {
   return "Happy holidays!"; 
 }
 
+function happyHolidaysTo(name) {
+  return `Happy holidays, 
+}
+
 function happyHolidaysTo(name, holiday) {
   return `Happy ${holiday}, ${name}!`;
 }
